ccffb161a7b328cd7e63e7bb08fdb9500e490209
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-03-24 09:51:54 -0400

    Fixed nextGeneration function

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
- // Initiate variables
- var columnCount = 20;
- var rowCount = 10;
- var shapeCount = 3;
- var iteration = 0;
- const counter = 5;
- var showValues = false;
+// Initiate variables
+var columnCount = 20;
+var rowCount = 10;
+var shapeCount = 3;
+var iteration = 0;
+const counter = 5;
+var showValues = false;
+var valuesMatrix = [[]]
 
- // Refer to shape-style.css
- var shapeList = [
+let testMatrix = [
+    [0, 1, 0, 3, 5],
+    [1, 1, 0, 3, 4],
+    [0, 0, 2, 3, 0],
+    [0, 1, 0, 3, 5],
+]
+
+// Refer to shape-style.css
+var shapeList = [
     '<div class ="shape">0</div>',
     '<div class ="shape">1</div>',
     '<div class ="shape">2</div>',
@@ -18,9 +26,9 @@
     '<div class ="shape">7</div>',
     '<div class ="shape">8</div>',
     '<div class ="shape">9</div>'
-     ]
+    ]
 
- // Generates a random integer between 0 and max
+// Generates a random integer between 0 and max
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -45,66 +53,69 @@ function generateRandomMatrix (rowCount, columnCount, maxValue) {
 
  
  // Get the values for the neighboring cells for a given cell in a matrix
- function getNeighbors(x, y, matrix) {
+ function getNeighbors(i, j, matrix) {
      let rowCount = matrix.length;
      let columnCount = matrix[0].length;
+     // Note that in a matrix [1,0] translates into 1 down, and 0 right. 
      let directions = [[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1],[0,-1],[1,-1]];
      let neighbors = [];
      
-     for (i = 0; i < directions.length; i++) {
+     for (d = 0; d < directions.length; d++) {
              
-         // To achieve a true modulo operator like python, I had to create the following equation
-         let tempX = directions[i][0] + x;
-         let newX = (tempX % columnCount + columnCount) % columnCount;
-         let tempY = directions[i][1] + y;
-         let newY = (tempY % rowCount + rowCount) % rowCount;
+        // Wrapping around the matrix in case of edge members
+        let tempI = directions[d][0] + i;
+        let tempJ = directions[d][1] + j;
+        if (tempI >= 0 && tempJ >= 0 && tempI < rowCount && tempJ < columnCount) {
+            neighbors.push( matrix[tempI][tempJ])
+        }
+        //  let newI = (tempI + rowCount) % rowCount;
          
-         neighbors[i] = matrix[newY][newX];	         
+        //  let newJ = (tempJ  + columnCount) % columnCount;
+         
+         	         
      }
 
      // Return results
      return neighbors;
  }
  
- // Return the most common item in a list
- function mostCommon(list) {
-     let map = {};
-     let mostCommonElement = list[0];
-     
-     for(var i = 0; i<list.length; i++){
-         if(!map[list[i]]){
-             map[list[i]]=1;
-         }else{
-             ++map[list[i]];
-             if(map[list[i]]>map[mostCommonElement]){
-                 mostCommonElement = list[i];
-             }
-         }
-     }
+// Return the most common item in a list
+function mostCommon(list) {
+    let map = {};
+    let mostCommonElement = list[0];
+    
+    for(var i = 0; i<list.length; i++){
+        if(!map[list[i]]){
+            map[list[i]]=1;
+        }else{
+            ++map[list[i]];
+            if(map[list[i]]>map[mostCommonElement]){
+                mostCommonElement = list[i];
+            }
+        }
+    }
 
-     // Return results
-     return mostCommonElement;
- }
+    // Return results
+    return mostCommonElement;
+}
  
- // Create the next generation based on an initial matrix of values
- function nextGeneration (matrix){
-     let rowCount = matrix.length;
-     let columnCount = matrix[0].length;
-     let newMatrix = [...matrix];
-     let mostCommonElement = 0;
-     let neighbors = [];
-     
-     for (let j = 0; j < rowCount; j++) {
-         for (let i = 0; i < columnCount; i++) {
- 
-         neighbors = getNeighbors (i, j, matrix);
-         mostCommonElement = mostCommon (neighbors);
-         newMatrix[j][i] = mostCommonElement;
-             
-     }
-     }
-     return newMatrix;
- }
+// Create the next generation based on an initial matrix of values
+function nextGeneration (matrix){
+    let rowCount = matrix.length;
+    let columnCount = matrix[0].length;
+    let newMatrix = [...matrix];
+    let mostCommonElement = 0;
+    let neighbors = [];
+    
+    for (let i = 0; i < rowCount; i++) {
+        for (let j = 0; j < columnCount; j++) {
+            neighbors = getNeighbors (i, j, matrix);
+            mostCommonElement = mostCommon (neighbors);
+            newMatrix[i][j] = mostCommonElement;  
+        }
+    }
+    return newMatrix;
+}
  
 // Convert a matrix to a list
 function matrixToList (matrix) {
@@ -117,45 +128,43 @@ function matrixToList (matrix) {
             itemIndex = j + i*columnCount;
             list[itemIndex] = matrix[i][j];
         }
-     }
-     return list;
- }
+        }
+    return list;
+}
  
- // Draw a grid based on the input matrix of colors 
- function matrixToGrid(matrix) {
-     let rowCount = matrix.length;
-     let columnCount = matrix[0].length;
-     let gridCount = columnCount*rowCount;
-     let gridHTML = "";
-     let gridCSSColumns = "";
-     let gridCSSRows = "";
-     let value = 0;
-     let valueList = matrixToList (matrix);
-     
+// Draw a grid based on the input matrix of colors 
+function matrixToGrid(matrix) {
+    let rowCount = matrix.length;
+    let columnCount = matrix[0].length;
+    let gridCount = columnCount*rowCount;
+    let gridHTML = "";
+    let gridCSSColumns = "";
+    let gridCSSRows = "";
+    let value = 0;
+    let valueList = matrixToList (matrix);
+    
     // Generate the HTML code
     for (let i = 0; i < gridCount; i++) {
         value = valueList[i] ;
         gridHTML += '<div id = pixel-'+ i + ' class="grid-item value-'+value+'">'+ shapeList[value] + '</div>';   
     }
-     
+        
     // Generate the CSS code | Column style
     for (let i = 0; i < columnCount; i++) {
         gridCSSColumns += "auto ";
     }
-    
+
     // Generate the CSS code | Row style
     for (let i = 0; i < rowCount; i++) {
         gridCSSRows += "auto ";
     }
-     
+        
     // Insert the HTML and CSS codes
     document.getElementById("my-grid").innerHTML = gridHTML;
     document.getElementById("my-grid").style.gridTemplateColumns = gridCSSColumns;
     document.getElementById("my-grid").style.gridTemplateRows = gridCSSRows;
-    
-    // document.getElementById("grid-item").style.fontSize = 50%;
-    // document.getElementById("demo").innerHTML = getNeighbors(0, 0, matrix);
- }
+
+}
  
  // FUNCTIONS TO USE AS BUTTON ON THE PAGE 
  
@@ -199,8 +208,8 @@ function removeVariance() {
  
 function nextIteration() {
     iteration += 1;
-    colorsMatrix = nextGeneration (colorsMatrix);
-    matrixToGrid (colorsMatrix);
+    valuesMatrix = nextGeneration (valuesMatrix);
+    matrixToGrid (valuesMatrix);
 }
  
 function reset() {
@@ -234,8 +243,8 @@ function toggleValues() {
 
 // Generate a new matrix and reload
 function refreshGrid() {
-    let newMatrix = generateRandomMatrix (rowCount, columnCount, shapeCount);
-    matrixToGrid (newMatrix)
+    valuesMatrix = generateRandomMatrix (rowCount, columnCount, shapeCount);
+    matrixToGrid (valuesMatrix)
 
 }
 
